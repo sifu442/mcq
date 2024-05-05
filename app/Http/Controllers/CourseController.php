3c013d22967b602e3b640413050036d52c56ab06
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-05 16:56:58 +0000

    May 5, 2024, 10:56 PM

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,26 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     public function show(Course $course)
-{
-    $subjects = collect();
-    foreach ($course->exams as $exam) {
-        foreach ($exam->questions as $question) {
-            $subjects->push($question->subject);
+    {
+        $subjects = collect();
+        foreach ($course->exams as $exam) {
+            foreach ($exam->questions as $question) {
+                $subjects->push($question->subject);
+            }
         }
+        $uniqueSubjects = $subjects->unique();
+
+        $exams = $course->exams;
+
+        $currentDate = Carbon::now();
+        $futureDate = $currentDate->addDays(4)->toDateString();
+
+        return view('course.show', ['course' => $course, 'subjects' => $uniqueSubjects, 'exams' => $exams]);
     }
-    $uniqueSubjects = $subjects->unique();
-
-    $exams = $course->exams;
-
-    return view('course.show', ['course' => $course, 'subjects' => $uniqueSubjects, 'exams' => $exams]);
-}
-
-
-
 }
