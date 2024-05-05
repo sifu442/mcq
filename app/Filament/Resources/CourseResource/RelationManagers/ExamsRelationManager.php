dcba77eef1fd0da2d9ed19d69fbf03449759d330
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-05 09:20:20 +0000

    May 5, 2024, 3:20 PM

--- app/Filament/Resources/CourseResource/RelationManagers/ExamsRelationManager.php
+++ app/Filament/Resources/CourseResource/RelationManagers/ExamsRelationManager.php
@@ -7,7 +7,9 @@ use Filament\Forms;
 use Filament\Tables;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Resources\RelationManagers\RelationManager;
@@ -19,7 +21,27 @@ class ExamsRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form->schema([
-            Forms\Components\TextInput::make('name')->required()->maxLength(255)
+            TextInput::make('name')->required()->translateLabel(),
+            Select::make('course_id')->relationship('course', 'title')->required(),
+            TextInput::make('syllabus')->required()->translateLabel(),
+            TextInput::make('duration')->required()->numeric()->suffix('Minutes')->translateLabel(),
+            TextInput::make('delay_days')->required()->numeric()->suffix('Days')->translateLabel(),
+            TextInput::make('available_for_hours')->required()->numeric()->suffix('Hours')->translateLabel(),
+            Select::make('score')
+                ->required()
+                ->options([
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                ]),
+            Select::make('penalty')
+                ->required()
+                ->options([
+                    '0.25' => '0.25',
+                    '0.50' => '0.50',
+                    '0.70' => '0.50',
+                    '1' => '1',
+                ]),
         ]);
     }
 
@@ -40,7 +62,7 @@ class ExamsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                //Tables\Actions\CreateAction::make(),
+                Tables\Actions\CreateAction::make(),
                 Tables\Actions\AttachAction::make(),
             ])
             ->actions([Tables\Actions\EditAction::make(), Tables\Actions\DeleteAction::make()])
