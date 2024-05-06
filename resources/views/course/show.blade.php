0a9ec3ceab328125c71a1ef6cc0cbc8c3736619e
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-06 15:55:14 +0000

    May 6, 2024, 9:55 PM

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -60,7 +60,7 @@
                     </button>
                 </h2>
                 <div id="accordion-collapse-body-{{ $exam->id }}" class="hidden" aria-labelledby="accordion-collapse-heading-{{ $exam->id }}">
-                    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                    <div class="p-5 border border-b-0 border-primary dark:border-gray-700 dark:bg-gray-900">
                         <p class="mb-2 text-gray-500 dark:text-gray-400">{{ $exam->syllabus }}</p>
                     </div>
                 </div>
