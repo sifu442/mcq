736a42c0904e22f1ffde1012d9757460b371bf91
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-06 15:33:43 +0000

    May 6, 2024, 9:33 PM

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -46,10 +46,10 @@
 
         <div id="accordion-collapse" data-accordion="collapse">
             @foreach ($exams as $exam)
-                <h2 id="accordion-collapse-heading-1">
+                <h2 id="accordion-collapse-heading-{{ $exam->id }}">
                     <button type="button"
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-                        data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+                        data-accordion-target="#accordion-collapse-body-{{ $exam->id }}" aria-expanded="true"
                         aria-controls="accordion-collapse-body-1">
                         <span>{{ $exam->name }}</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
@@ -59,7 +59,7 @@
                         </svg>
                     </button>
                 </h2>
-                <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div id="accordion-collapse-body-{{ $exam->id }}" class="hidden" aria-labelledby="accordion-collapse-heading-1">
                     <div class="p-5 border border-bt-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                         <p class="mb-2 text-gray-500 dark:text-gray-400">{{ $exam->syllabus }}</p>
                     </div>
