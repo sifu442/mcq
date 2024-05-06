ebe5b1a719607042bae2d2553474d3020dca79aa
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-06 15:42:59 +0000

    May 6, 2024, 9:42 PM

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -51,7 +51,7 @@
                     class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                     data-accordion-target="#accordion-collapse-body-{{ $loop->iteration }}" aria-expanded="{{ $loop->first ? 'true' : 'false' }}"
                     aria-controls="accordion-collapse-body-{{ $loop->iteration }}">
-                    <span>{{ $exam->title }}</span>
+                    <span class="text-red-500">{{ $exam->title }}</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
