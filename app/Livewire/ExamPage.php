86913861b923ea83945e75b34652ff4f0b235b0f
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-09 06:08:07 +0000

    May 9, 2024, 12:08 PM

--- app/Livewire/ExamPage.php
+++ app/Livewire/ExamPage.php
@@ -28,26 +28,26 @@ class ExamPage extends Component
         if ($this->examSubmitted) {
             return;
         }
-    
+
         $user = Auth::user(); // Get the authenticated user
-    
+
         if ($this->exam) {
             $responseData = [];
             foreach ($this->exam->questions as $question) {
                 $correctAnswer = collect($question->options)->where('is_correct', true)->pluck('options')->first();
                 $userAnswer = $this->answers[$question->id] ?? null;
                 $isCorrect = $userAnswer === $correctAnswer;
-            
+
                 // Assuming options is a collection of arrays with 'options' and 'is_correct' keys
                 $options = collect($question->options)->pluck('options')->toArray();
-            
+
                 $responseData[] = [
                     'question' => $question->title,
                     'options' => $options,
                     'user_input' => $userAnswer,
                     'correct_answer' => $correctAnswer,
                 ];
-            
+
                 if ($isCorrect) {
                     $this->totalScore += 1; // Increment score for correct answer
                 } elseif ($userAnswer !== null) {
@@ -58,10 +58,10 @@ class ExamPage extends Component
                     $this->totalScore -= 0.25;
                 }
             }
-            
+
             // Ensure total score is non-negative
             $totalScore = max(0, $this->totalScore);
-    
+
             ExamResponse::create([
                 'user_id' => $user->id,
                 'exam_id' => $this->exam->id,
@@ -69,18 +69,18 @@ class ExamPage extends Component
                 'total_score' => $totalScore,
             ]);
         }
-    
+
         $this->examSubmitted = true;
         $this->reset(['answers']);
-    
+
         return redirect()->route('exam-results', ['examId' => $this->examId]);
     }
-    
 
 
-    
+
+
 
     public function render(){
-        return view('livewire.exam-page')->extends('layouts.app');
+        return view('livewire.exam-page')->layout('layouts.app');
     }
 }
