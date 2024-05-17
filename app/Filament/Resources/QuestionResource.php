cdec9114ee07eee25c438e9ecf8c208c1cb0e626
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-17 06:06:56 +0000

    May 17, 2024, 12:06 PM

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -1,88 +1,87 @@
 <?php
-namespace App\Filament\Resources\ExamResource\RelationManagers;
 
-use stdClass;
-use Filament\Forms;
-use Filament\Tables;
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuestionResource\Pages;
+use App\Filament\Resources\QuestionResource\RelationManagers;
 use App\Models\Question;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Filament\Forms\Components\Select;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Repeater;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Filament\Resources\RelationManagers\RelationManager;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor;
 
-class QuestionResource extends RelationManager
+class QuestionResource extends Resource
 {
-    protected static string $relationship = 'questions';
+    protected static ?string $model = Question::class;
 
-    public function form(Form $form): Form
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
     {
         return $form->schema([
+            TextInput::make('title')
+                //->label('Select Subject')
+                ->required()
+                ->columnSpanFull(),
             Select::make('subject_id')
                 ->relationship('subject', 'name')
-                ->createOptionForm([TextInput::make('name')->required()])
+                ->createOptionForm([
+                    TextInput::make('name')->required()
+                ])
                 ->required(),
-            TextInput::make('exam_name'),
-            TextInput::make('post'),
-            DatePicker::make('date'),
-            RichEditor::make('title')->required()->maxLength(255)->columnSpanFull(),
             Repeater::make('options')
                 ->required()
                 ->deletable(false)
                 ->defaultItems(4)
                 ->maxItems(4)
                 ->schema([
-                    TextInput::make('option'),
-                    Checkbox::make('is_correct')->fixIndistinctState()->name('Correct Answer')
+                    TextInput::make('options'),
+                    Checkbox::make('is_correct')
+                        ->fixIndistinctState()
+                        ->name('Correct Answer'),
                 ])
                 ->columnSpanFull(),
-            RichEditor::make('explanation')->columnSpanFull()
         ]);
     }
 
-    public function table(Table $table): Table
+    public static function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('title')
-            ->columns([
-                TextColumn::make('index')
-                    ->label('Index')
-                    ->getStateUsing(function (stdClass $rowLoop, $livewire): string {
-                        $currentPage = method_exists($livewire, 'currentPage') ? $livewire->currentPage() : 1;
-                        return (string) ($rowLoop->iteration + $livewire->tableRecordsPerPage * ($currentPage - 1));
-                    }),
-                TextColumn::make('title'),
-            ])
+            ->columns([TextColumn::make('title')->searchable(), TextColumn::make('subject.name')->searchable()])
+            ->defaultSort('created_at', 'desc')
             ->filters([
-                // Add any necessary filters here
+                //
             ])
-            ->headerActions([
-                Tables\Actions\AttachAction::make()
-                    ->form([
-                        Select::make('question_id')
-                            ->label('Question')
-                            ->relationship('questions', 'title')
-                            ->searchable()
-                            ->createOptionForm([
-                                TextInput::make('title')->required()->label('Question Title'),
-                            ])
-                            ->required(),
-                    ]),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->actions([Tables\Actions\EditAction::make()])
+            ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+                //
+            ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestions::route('/'),
+            'create' => Pages\CreateQuestion::route('/create'),
+            'edit' => Pages\EditQuestion::route('/{record}/edit'),
+        ];
     }
 }
