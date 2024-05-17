41e63d257bee42af57554c566ecc9e55aedc41f1
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-17 05:08:18 +0000

    May 17, 2024, 11:08 AM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -64,35 +64,6 @@ class QuestionsRelationManager extends RelationManager
             ])
             ->headerActions([
                 Tables\Actions\AttachAction::make()
-                ->preloadRecordSelect()
-                    ->form([
-                        Select::make('subject_id')
-                            ->relationship('subject', 'name')
-                            ->createOptionForm([
-                                TextInput::make('name')->required()
-                            ])
-                            ->required(),
-                        TextInput::make('exam_name'),
-                        TextInput::make('post'),
-                        DatePicker::make('date'),
-                        RichEditor::make('title')
-                            ->required()
-                            ->maxLength(255)
-                            ->columnSpanFull(),
-                        Repeater::make('options')
-                            ->required()
-                            ->deletable(false)
-                            ->defaultItems(4)
-                            ->maxItems(4)
-                            ->schema([
-                                TextInput::make('option'),
-                                Checkbox::make('is_correct')
-                                    ->fixIndistinctState()
-                                    ->name('Correct Answer')
-                            ])
-                            ->columnSpanFull(),
-                        RichEditor::make('explanation')->columnSpanFull()
-                    ]),
             ])
             ->actions([Tables\Actions\EditAction::make(), Tables\Actions\DeleteAction::make()])
             ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
