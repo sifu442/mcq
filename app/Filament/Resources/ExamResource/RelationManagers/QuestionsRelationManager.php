7394293e6fc88e57d5a7596521d03fdf43496279
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-19 15:08:30 +0000

    May 19, 2024, 9:08 PM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -67,24 +67,24 @@ class QuestionsRelationManager extends RelationManager
                 Tables\Actions\AttachAction::make()
                 ->recordSelect(
                     fn (Select $select) => $select->createOptionForm([
-                        Select::make('subject_id')
-                            ->relationship('subject', 'name')
-                        ->required(),
-                TextInput::make('exam_name'),
-                TextInput::make('post'),
-                DatePicker::make('date'),
-                RichEditor::make('title')->required()->maxLength(255)->columnSpanFull(),
-                Repeater::make('options')
-                    ->required()
-                    ->deletable(false)
-                    ->defaultItems(4)
-                    ->maxItems(4)
-                        ->schema([
-                            TextInput::make('options'),
-                            Checkbox::make('is_correct')->fixIndistinctState()->name('Correct Answer')
-                ])
-                ->columnSpanFull(),
-            RichEditor::make('explanation')->columnSpanFull()
+                            Select::make('subject_id')
+                                ->relationship('subject', 'name')
+                                ->required(),
+                            TextInput::make('exam_name'),
+                            TextInput::make('post'),
+                            DatePicker::make('date'),
+                            RichEditor::make('title')->required()->maxLength(255)->columnSpanFull(),
+                            Repeater::make('options')
+                                ->required()
+                                ->deletable(false)
+                                ->defaultItems(4)
+                                ->maxItems(4)
+                                ->schema([
+                                    TextInput::make('options'),
+                                    Checkbox::make('is_correct')->fixIndistinctState()->name('Correct Answer')
+                                        ])
+                                ->columnSpanFull(),
+                            RichEditor::make('explanation')->columnSpanFull()
                     ]),
                 )
             ])
