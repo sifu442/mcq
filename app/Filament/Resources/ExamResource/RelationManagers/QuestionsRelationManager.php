8d6e9b44b15707f201d8f8cd3fa2efa6872a2739
Sifatul Islam <sifatulislam442@gmail.com>
2024-05-20 11:09:52 +0000

    May 20, 2024, 5:09 PM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -70,7 +70,7 @@ class QuestionsRelationManager extends RelationManager
             ->recordSelect(
                 fn (Select $select) => $select->createOptionForm([
                     Select::make('subject_id')
-                    ->options(Subject::all()->pluck('name', 'id'))
+                    ->relationship(name: 'subject', titleAttribute: 'name')
                 ]),
             )
             ])
