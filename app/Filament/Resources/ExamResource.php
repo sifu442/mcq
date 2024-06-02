fd0cf045506816ea2761d2ab55ae09f7a8020a59
Sifatul Islam <sifatulislam442@gmail.com>
2024-06-02 04:53:32 +0000

    Jun 2, 2024, 10:53 AM

--- app/Filament/Resources/ExamResource.php
+++ app/Filament/Resources/ExamResource.php
@@ -154,27 +154,24 @@ class ExamResource extends Resource
                         ->columnSpanFull(),
                         ])
                         ->action(function (array $data) {
-                            DB::transaction(function () use ($data) {
-                                // Create the new exam
-                                $newExam = Exam::create([
-                                    'name' => $data['name'],
-                                    'course_id' => $data['course_id'],
-                                    'duration' => $data['duration'],
-                                    'delay_days' => $data['delay_days'],
-                                    'available_for_hours' => $data['available_for_hours'],
-                                    'score' => $data['score'],
-                                    'penalty' => $data['penalty'],
-                                    'syllabus' => $data['syllabus'],
-                                ]);
+                            // Create new exam
+                            $newExam = Exam::create([
+                                'name' => $data['name'],
+                                'course_id' => $data['course_id'],
+                                'duration' => $data['duration'],
+                                'delay_days' => $data['delay_days'],
+                                'available_for_hours' => $data['available_for_hours'],
+                                'score' => $data['score'],
+                                'penalty' => $data['penalty'],
+                                'syllabus' => $data['syllabus'],
+                            ]);
 
-                                // Ensure correct column name for exam_id
-                                $questions = Question::whereIn('exam_id', $data['exam_ids'])->get();
-                                foreach ($questions as $question) {
-                                    $question->replicate()->fill([
-                                        'exam_id' => $newExam->id,
-                                    ])->save();
-                                }
-                            });
+                            // Merge questions from selected exams
+                            $examIds = $data['exam_ids'];
+                            $questions = Question::whereIn('exam_id', $examIds)->get();
+                            $newExam->questions()->attach($questions->pluck('id'));
+
+                            return $newExam;
                         })
 
             ])
