9be6a355ed1311eefed0622b30068cb0a6286d88
Sifatul Islam <sifatulislam442@gmail.com>
2024-06-03 16:40:24 +0000

    Jun 3, 2024, 10:40 PM

--- resources/views/livewire/exam-results.blade.php
+++ resources/views/livewire/exam-results.blade.php
@@ -10,7 +10,7 @@
                         <li>{{ $questionData['question'] }}
                             <ul class="list-[upper-alpha]">
                                 @foreach ($questionData['options'] as $option)
-                                    <li class="flex items-center ps-4 border border-gray-200 rounded-md dark:border-gray-700 py-5 my-5 drop-shadow-lg {{ $questionData['user_input'] == $option ? ($questionData['user_input'] == $questionData['correct_answer'] ? 'bg-green-200' : 'bg-red-200') : ($questionData['correct_answer'] == $option ? 'bg-green-200' : 'bg-white') }}">
+                                    <li>
                                             {{ strip_tags($option) }}
                                     </li>
                                 @endforeach
