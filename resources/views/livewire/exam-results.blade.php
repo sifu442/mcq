c9b99af8cccb3ea60e60ec873b690196741c8127
Sifatul Islam <sifatulislam442@gmail.com>
2024-06-03 16:17:49 +0000

    Jun 3, 2024, 10:17 PM

--- resources/views/livewire/exam-results.blade.php
+++ resources/views/livewire/exam-results.blade.php
@@ -8,13 +8,11 @@
                 <div>
                     <ol class="list-decimal list-inside">
                         <li>{{ $questionData['question'] }}
-                            <ol type="A" class="list-inside ml-4">
+                            <ol class="list-decimal list-inside">
                                 @foreach ($questionData['options'] as $option)
-                                    <li class="">
-                                        <div
-                                            class="flex items-center ps-4 border border-gray-200 rounded-md dark:border-gray-700 py-5 my-5 drop-shadow-lg {{ $questionData['user_input'] == $option ? ($questionData['user_input'] == $questionData['correct_answer'] ? 'bg-green-200' : 'bg-red-200') : ($questionData['correct_answer'] == $option ? 'bg-green-200' : 'bg-white') }}">
+                                    <li
+                                    class="flex items-center ps-4 border border-gray-200 rounded-md dark:border-gray-700 py-5 my-5 drop-shadow-lg {{ $questionData['user_input'] == $option ? ($questionData['user_input'] == $questionData['correct_answer'] ? 'bg-green-200' : 'bg-red-200') : ($questionData['correct_answer'] == $option ? 'bg-green-200' : 'bg-white') }}">
                                             {{ strip_tags($option) }}
-                                        </div>
                                     </li>
                                 @endforeach
                             </ol>
