2be2531957a850b898a68ca892ee7b4d5b12096e
sifu442 <sifatulislam442@gmail.com>
2024-06-06 08:14:11 +0000

    Jun 6, 2024, 2:14 PM

--- app/Filament/Resources/PurchaseResource/Pages/ListPurchases.php
+++ app/Filament/Resources/PurchaseResource/Pages/ListPurchases.php
@@ -9,11 +9,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListPurchases extends ListRecords
 {
     protected static string $resource = PurchaseResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
