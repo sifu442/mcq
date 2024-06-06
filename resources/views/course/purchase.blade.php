35e16528952725943691e726dff399a99b6cb080
sifu442 <sifatulislam442@gmail.com>
2024-06-06 06:33:04 +0000

    Jun 6, 2024, 12:33 PM

--- resources/views/course/purchase.blade.php
+++ resources/views/course/purchase.blade.php
@@ -1,40 +1,41 @@
 <x-app-layout>
-    <div class="">
-        <div class="">
-            <div class="p-8">
-                <h2 class="block mt-1 text-lg leading-tight font-medium text-black">{{ $course->name }}</h2>
-                <p class="mt-2 text-gray-500">Course: {{ $course->title }}</p>
-                <p class="mt-2 text-gray-500">Fee: {{ $course->price }} TK</p>
-                <p class="mt-2 text-gray-500">Student: {{ $user->name }}</p>
-                <p class="mt-2 text-gray-500">Roll: {{ $user->id }}</p>
-                <form method="POST" action="{{ route('course.purchase', ['course' => $course->id]) }}">
-                    @csrf
-                    <div class="mt-4">
-                        <label for="countries"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
-                            option</label>
-                        <select id="countries"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose a payment method</option>
-                            <option value="Bkash">Bkash</option>
-                            <option value="Nagad">Nagad</option>
-                            <option value="Rocket">Rocket</option>
-                        </select>
-                    </div>
-                    <div class="mt-4">
-                        <div class="relative z-0 w-full mb-5 group">
-                            <input type="number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (019*******)</label>
-                        </div>
-                    </div>
-                    <div class="mt-6">
-                        <button type="submit"
-                            class="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Purchase
-                        </button>
-                    </div>
-                </form>
+    <div>
+        <h2 class="block mt-1 text-lg leading-tight font-medium text-black">{{ $course->name }}</h2>
+        <p class="mt-2 text-gray-500">Course: {{ $course->title }}</p>
+        <p class="mt-2 text-gray-500">Fee: {{ $course->price }} TK</p>
+        <p class="mt-2 text-gray-500">Student: {{ $user->name }}</p>
+        <p class="mt-2 text-gray-500">Roll: {{ $user->id }}</p>
+    </div>
+    <form method="POST" action="{{ route('course.purchase', ['course' => $course->id]) }}">
+        @csrf
+        <div class="mt-4">
+            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
+                option</label>
+            <select id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose a payment method</option>
+                <option value="Bkash">Bkash</option>
+                <option value="Nagad">Nagad</option>
+                <option value="Rocket">Rocket</option>
+            </select>
+        </div>
+        <div class="mt-4">
+            <div class="relative z-0 w-full mb-5 group">
+                <input type="number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" " required />
+                <label for="floating_phone"
+                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone
+                    number (019*******)</label>
             </div>
         </div>
+        <div class="mt-6">
+            <button type="submit"
+                class="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Purchase
+            </button>
+        </div>
+    </form>
+    </div>
     </div>
 </x-app-layout>
