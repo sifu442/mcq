f9a5a30713a1099cf3397cdc5fa457a9371e8cf0
sifu442 <sifatulislam442@gmail.com>
2024-06-06 07:19:05 +0000

    Jun 6, 2024, 1:19 PM

--- resources/views/course/purchase.blade.php
+++ resources/views/course/purchase.blade.php
@@ -7,29 +7,31 @@
         <p class="mt-2 text-gray-500">Roll: {{ $user->id }}</p>
     </div>
 
-    <div class="payment-details flex items-center">
-        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mr-5">
+    <div class="payment-details flex justify-between pt-5">
+        <div href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mcqjob.com/uploads/others/Bkash%20(Personal).svg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">01886886640</p>
             </div>
-        </a>
-        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mr-5">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
+        </div>
+        <div href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mcqjob.com/uploads/others/Bkash%20(Personal).svg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">01886886640</p>
             </div>
-        </a>
-        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
+        </div>
+        <div href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mcqjob.com/uploads/others/Bkash%20(Personal).svg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">01886886640</p>
             </div>
-        </a>
+        </div>
     </div>
 
     <form method="POST" action="{{ route('course.purchase', ['course' => $course->id]) }}">
