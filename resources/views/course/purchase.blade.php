1330f932819ba22a50a74a40b30c7e6630702d07
sifu442 <sifatulislam442@gmail.com>
2024-06-06 06:13:12 +0000

    Jun 6, 2024, 12:13 PM

--- resources/views/course/purchase.blade.php
+++ resources/views/course/purchase.blade.php
@@ -1,29 +1,39 @@
 <x-app-layout>
-
+    <div class="">
         <div class="">
-            <div class="flex">
-                <div class="p-8">
-                    <h2 class="block mt-1 text-lg leading-tight font-medium text-black">{{ $course->name }}</h2>
-                    <p class="mt-2 text-gray-500">Fee: {{ $course->price }}</p>
-                    <p class="mt-2 text-gray-500">User: {{ $user->name }}</p>
-                    <p class="mt-2 text-gray-500">User ID: {{ $user->id }}</p>
-                    <form method="POST" action="{{ route('course.purchase', ['course' => $course->id]) }}">
-                        @csrf
-                        <div class="mt-4">
-                            <label for="payment_method" class="block text-sm font-medium text-gray-700">Payment Method</label>
-                            <input type="text" name="payment_method" id="payment_method" class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-                        <div class="mt-4">
-                            <label for="phone_number" class="block text-sm font-medium text-gray-700">Phone Number</label>
-                            <input type="text" name="phone_number" id="phone_number" class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-                        <div class="mt-6">
-                            <button type="submit" class="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Purchase
-                            </button>
-                        </div>
-                    </form>
-                </div>
+            <div class="p-8">
+                <h2 class="block mt-1 text-lg leading-tight font-medium text-black">{{ $course->name }}</h2>
+                <p class="mt-2 text-gray-500">Course: {{ $course->title }}</p>
+                <p class="mt-2 text-gray-500">Fee: {{ $course->price }}</p>
+                <p class="mt-2 text-gray-500">Student: {{ $user->name }}</p>
+                <p class="mt-2 text-gray-500">Roll: {{ $user->id }}</p>
+                <form method="POST" action="{{ route('course.purchase', ['course' => $course->id]) }}">
+                    @csrf
+                    <div class="mt-4">
+                        <label for="countries"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
+                            option</label>
+                        <select id="countries"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Choose a country</option>
+                            <option value="Bkash">Bkash</option>
+                            <option value="Nagad">Nagad</option>
+                            <option value="Rocket">Rocket</option>
+                        </select>
+                    </div>
+                    <div class="mt-4">
+                        <label for="phone_number" class="block text-sm font-medium text-gray-700">Phone Number</label>
+                        <input type="text" name="phone_number" id="phone_number"
+                            class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                    </div>
+                    <div class="mt-6">
+                        <button type="submit"
+                            class="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Purchase
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
 </x-app-layout>
