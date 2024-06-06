24e5d55fc9248eacac420f3f078f9ea352fa1544
sifu442 <sifatulislam442@gmail.com>
2024-06-06 07:24:34 +0000

    Jun 6, 2024, 1:24 PM

--- resources/views/course/purchase.blade.php
+++ resources/views/course/purchase.blade.php
@@ -9,7 +9,7 @@
 
     <div class="payment-details flex justify-between pt-5">
         <div href="#" class="flex flex-col items-center  border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://download.logo.wine/logo/BKash/BKash-Icon-Logo.wine.png" alt="">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://download.logo.wine/logo/BKash/BKash-Icon-Logo.wine.png" alt="Bkash-logo">
             <div class="flex flex-col justify-between p-4 leading-normal">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
@@ -17,17 +17,17 @@
             </div>
         </div>
         <div href="#" class="flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mcqjob.com/uploads/others/Bkash%20(Personal).svg" alt="">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://download.logo.wine/logo/Nagad/Nagad-Vertical-Logo.wine.png" alt="Nagad-logo">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nagad</h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">01886886640</p>
             </div>
         </div>
         <div href="#" class="flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mcqjob.com/uploads/others/Bkash%20(Personal).svg" alt="">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://seeklogo.com/images/D/dutch-bangla-rocket-logo-B4D1CC458D-seeklogo.com.png" alt="Rocket">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bkash</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Rocket</h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Marchent</p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">01886886640</p>
             </div>
