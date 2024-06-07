40b55cf0f4cff53bd4209bcec41aa38ae512a0f5
sifu442 <sifatulislam442@gmail.com>
2024-06-07 07:23:33 +0000

    Jun 7, 2024, 1:23 PM

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -67,7 +67,6 @@ class QuestionResource extends Resource
                 ])
                 ->columnSpanFull(),
                 TinyEditor::make('explanation')
-                ->simple()
                 ->columnSpanFull()
         ]);
     }
