c7e8b0a8aa1540497eec383228802eed5fb7661f
sifu442 <sifatulislam442@gmail.com>
2024-06-09 07:01:25 +0000

    Jun 9, 2024, 1:01 PM

--- resources/views/filament/forms/components/tiny-mce-field.blade.php
+++ resources/views/filament/forms/components/tiny-mce-field.blade.php
@@ -7,22 +7,5 @@
     :required="$isRequired()"
     :statePath="$getStatePath()"
 >
-    <div
-        x-data="{
-            editor: null,
-            init() {
-                this.editor = tinymce.init({
-                    target: this.$refs.editor,
-                    {{ $applyStateBindingModifiers('init') }}={{ $getStatePath() }},
-                    setup: (editor) => {
-                        // Additional TinyMCE configuration options
-                    },
-                    // Additional TinyMCE configuration options
-                })
-            }
-        }"
-        x-init="init()"
-    >
-        <textarea x-ref="editor" {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"></textarea>
-    </div>
+    <!-- Your TinyMCE field code here -->
 </x-forms::field-wrapper>
