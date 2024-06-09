44923fd34e4a2b47796eb251f5d359335a4589eb
sifu442 <sifatulislam442@gmail.com>
2024-06-09 06:27:52 +0000

    Jun 9, 2024, 12:27 PM

--- resources/views/filament/forms/components/tiny-mce-editor.blade.php
+++ resources/views/filament/forms/components/tiny-mce-editor.blade.php
@@ -3,7 +3,7 @@
 
     @once
         @push('scripts')
-            <script src="https://cdn.tiny.cloud/1/lrii0uxtuwcwtm989pioll20tuxfxuq3dyuyf0tzbqs7urbz/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+            <script src="https://cdn.tiny.cloud/1/your-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
         @endpush
     @endonce
 
@@ -12,8 +12,21 @@
             document.addEventListener('DOMContentLoaded', function () {
                 tinymce.init({
                     selector: '#tiny-mce-editor-{{ $getId() }}',
-                    plugins: 'code table lists',
-    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+                    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+tinycomments_mode: 'embedded',
+tinycomments_author: 'Author name',
+mergetags_list: [
+  { value: 'First.Name', title: 'First Name' },
+  { value: 'Email', title: 'Email' },
+],
+ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+                    setup: function (editor) {
+                        editor.on('Change', function () {
+                            @this.set('{{ $getStatePath() }}', editor.getContent());
+                        });
+                    },
+                    readonly: false, // Ensure the editor is writable
                 });
             });
         </script>
