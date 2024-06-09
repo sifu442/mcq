4b8c88a19abad8ce04c48fc7627a6db2c82a3781
sifu442 <sifatulislam442@gmail.com>
2024-06-09 07:10:12 +0000

    Jun 9, 2024, 1:10 PM

--- resources/views/forms/components/tiny-mce-field.blade.php
+++ resources/views/forms/components/tiny-mce-field.blade.php
@@ -1,8 +1,17 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
->
-    <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
-        <!-- Interact with the `state` property in Alpine.js -->
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
+    <div x-data="{
+        editor: null,
+        init() {
+            this.editor = tinymce.init({
+                target: this.$refs.editor,
+                {{ $applyStateBindingModifiers('init') }} = {{ $getStatePath() }},
+                setup: (editor) => {
+                    // Additional TinyMCE configuration options
+                },
+                // Additional TinyMCE configuration options
+            })
+        }
+    }" x-init="init()">
+        <textarea x-ref="editor" {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"></textarea>
     </div>
 </x-dynamic-component>
