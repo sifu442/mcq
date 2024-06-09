559f36f02e96948365d346f8aac20be224422638
sifu442 <sifatulislam442@gmail.com>
2024-06-09 07:17:31 +0000

    Jun 9, 2024, 1:17 PM

--- resources/views/forms/components/tiny-mce-field.blade.php
+++ resources/views/forms/components/tiny-mce-field.blade.php
@@ -7,27 +7,36 @@
     :required="$isRequired()"
     :statePath="$getStatePath()"
     >
-    <div x-data="{
+    <div
+    x-data="{
         editor: null,
-        init() {
-            this.editor = tinymce.init({
-                target: this.$refs.editor,
-                {{ $applyStateBindingModifiers('init') }}={{ $getStatePath() }},
-                menubar: true,
-                plugins: [
-                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-                    'anchor', 'searchreplace', 'visualblocks', 'code',
-                    'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
-                ],
-                toolbar: 'undo redo | blocks | ' +
-                    'bold italic forecolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            })
+        isTinyMCELoaded: false,
+        initEditor() {
+            if (this.isTinyMCELoaded) {
+                this.editor = tinymce.init({
+                    target: this.$refs.editor,
+                    {{ $applyStateBindingModifiers('init') }}={{ $getStatePath() }},
+                    menubar: true,
+                    plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+                        'anchor', 'searchreplace', 'visualblocks', 'code',
+                        'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                    ],
+                    toolbar: 'undo redo | blocks | ' +
+                        'bold italic forecolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                })
+            }
         }
     }"
-    x-init="init()"
+    x-init="
+        tinymce.PluginManager.load('tinymce/tinymce.min.js', function() {
+            isTinyMCELoaded = true;
+            initEditor();
+        });
+    "
 >
     <textarea x-ref="editor" {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"></textarea>
 </div>
