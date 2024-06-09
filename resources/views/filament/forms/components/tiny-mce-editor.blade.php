3baeb86884b96d08ea2556ae1bc6bd37ad485df4
sifu442 <sifatulislam442@gmail.com>
2024-06-09 06:34:39 +0000

    Jun 9, 2024, 12:34 PM

--- resources/views/filament/forms/components/tiny-mce-editor.blade.php
+++ resources/views/filament/forms/components/tiny-mce-editor.blade.php
@@ -3,7 +3,7 @@
 
     @once
         @push('scripts')
-            <script src="https://cdn.tiny.cloud/1/your-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+            <script src="https://cdn.tiny.cloud/1/lrii0uxtuwcwtm989pioll20tuxfxuq3dyuyf0tzbqs7urbz/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
         @endpush
     @endonce
 
@@ -12,13 +12,6 @@
             document.addEventListener('DOMContentLoaded', function () {
                 tinymce.init({
                     selector: '#tiny-mce-editor-{{ $getId() }}',
-                    theme: "silver",
-        plugins: [ "image code table link media codesample"],
-        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | media | table',
-        //paste Core plugin options
-        paste_block_drop: false,
-        paste_data_images: true,
-        paste_as_text: true,
                     setup: function (editor) {
                         editor.on('Change', function () {
                             @this.set('{{ $getStatePath() }}', editor.getContent());
