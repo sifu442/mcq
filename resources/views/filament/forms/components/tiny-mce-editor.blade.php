7afa9adc1aa6d225dc61b087b333e32b814438a0
sifu442 <sifatulislam442@gmail.com>
2024-06-09 06:29:44 +0000

    Jun 9, 2024, 12:29 PM

--- resources/views/filament/forms/components/tiny-mce-editor.blade.php
+++ resources/views/filament/forms/components/tiny-mce-editor.blade.php
@@ -12,15 +12,13 @@
             document.addEventListener('DOMContentLoaded', function () {
                 tinymce.init({
                     selector: '#tiny-mce-editor-{{ $getId() }}',
-                    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-tinycomments_mode: 'embedded',
-tinycomments_author: 'Author name',
-mergetags_list: [
-  { value: 'First.Name', title: 'First Name' },
-  { value: 'Email', title: 'Email' },
-],
-ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+                    theme: "silver",
+        plugins: [ "image code table link media codesample"],
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | media | table',
+        //paste Core plugin options
+        paste_block_drop: false,
+        paste_data_images: true,
+        paste_as_text: true,
                     setup: function (editor) {
                         editor.on('Change', function () {
                             @this.set('{{ $getStatePath() }}', editor.getContent());
