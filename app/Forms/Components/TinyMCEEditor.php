19641fd046724a08a73714e3697cb8fb973e31c9
sifu442 <sifatulislam442@gmail.com>
2024-06-09 05:57:07 +0000

    Jun 9, 2024, 11:57 AM

---
+++ app/Forms/Components/TinyMCEEditor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Forms\Components;
+
+use Filament\Forms\Components\Field;
+
+class TinyMCEEditor extends Field
+{
+    protected string $view = 'filament.forms.components.tiny-mce-editor';
+}
