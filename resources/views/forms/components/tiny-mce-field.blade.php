889f0b6a3f5a0ea9bc97e1affc2529ba0a95f651
sifu442 <sifatulislam442@gmail.com>
2024-06-09 07:19:52 +0000

    Jun 9, 2024, 1:19 PM

--- resources/views/forms/components/tiny-mce-field.blade.php
+++ resources/views/forms/components/tiny-mce-field.blade.php
@@ -3,11 +3,9 @@
     :field="$field"
     >
     <div
-    x-data="{
-        editor: null,
-        isTinyMCELoaded: false,
-        initEditor() {
-            if (this.isTinyMCELoaded) {
+        x-data="{
+            editor: null,
+            init: function() {
                 this.editor = tinymce.init({
                     target: this.$refs.editor,
                     {{ $applyStateBindingModifiers('init') }}={{ $getStatePath() }},
@@ -24,15 +22,11 @@
                     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                 })
             }
-        }
-    }"
-    x-init="
-        tinymce.PluginManager.load('tinymce/tinymce.min.js', function() {
-            isTinyMCELoaded = true;
-            initEditor();
-        });
-    "
->
+        }"
+        x-init="init()"
+    >
+        <textarea x-ref="editor" {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"></textarea>
+    </div>
     <textarea x-ref="editor" {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"></textarea>
 </div>
 </x-dynamic-component>
