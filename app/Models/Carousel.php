e15f5886e62f9604ea72a718eb4877b90034f026
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-17 04:43:21 +0000

    Aug 17, 2024, 10:43 AM

--- app/Models/Carousel.php
+++ app/Models/Carousel.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carousel extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['image_path', 'caption'];
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url($this->image_path);
+    }
 }
