0e3549aaa103ef66999814c03fb048da7f7a8107
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-17 07:45:27 +0000

    Aug 17, 2024, 1:45 PM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -147,10 +147,10 @@ class QuestionsRelationManager extends RelationManager
                                     ->pluck('title', 'id')
                                     ->toArray()
                             )
-                            ->afterStateUpdated(function (callable $set, $state, $get) {
-                                // If no question is selected, copy the search text to the title field
-                                if (!$state) {
-                                    $set('title', $get('search-question'));
+                            ->afterStateUpdated(function (Set $set, $state) {
+                                $question = Question::find($state);
+                                if ($question) {
+                                    $set('title', $question->title);
                                 }
                             })
                             ->getOptionLabelUsing(fn ($value): ?string => Question::find($value)?->title)
