023da9fe47c0e01aa524ef388b9f577842ded312
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-20 16:45:29 +0000

    Aug 20, 2024, 10:45 PM

--- resources/views/forms/components/custom-search.blade.php
+++ resources/views/forms/components/custom-search.blade.php
@@ -1,33 +1,38 @@
-<div x-data="{
-    state: @entangle('state'),
-    searchResults: @entangle('searchResults'),
-    initEditor() {
-        ClassicEditor
-            .create($refs.content)
-            .then(editor => {
-                this.editor = editor;
-                editor.model.document.on('change:data', () => {
-                    this.state = editor.getData();
-                });
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
+    <div wire:ignore x-data="{
+        state: $wire.entangle('{{ $getStatePath() }}'),
+        searchResults: [],
+        initEditor() {
+            ClassicEditor
+                .create($refs.content)
+                .then(editor => {
+                    this.editor = editor;
 
-                window.addEventListener('set-editor-content', event => {
-                    this.editor.setData(event.detail.content);
+                    // Listen to changes in the editor
+                    editor.model.document.on('change:data', () => {
+                        $refs.content.value = editor.getData();
+                        this.state = editor.getData(); // Update state with editor data
+                        this.search(); // Perform search on data change
+                    });
+                })
+                .catch(error => {
+                    console.error(error);
                 });
-            })
-            .catch(error => {
-                console.error(error);
+        },
+        search() {
+            @this.call('getSearchResults').then(results => {
+                this.searchResults = results;
             });
-    },
-}"
-x-init="initEditor()"
->
-<textarea x-ref="content" x-bind:value="state"></textarea>
-<div class="search-results mt-2">
-    <ul>
-        <template x-for="result in searchResults" :key="result.id">
-            <li @click="$wire.setState(result.content)" x-text="result.title" class="cursor-pointer hover:bg-gray-200"></li>
-        </template>
-    </ul>
-</div>
-<script src="{{ asset('vendor/ckeditor5/build/ckeditor.js') }}"></script>
-</div>
+        }
+    }" x-init="initEditor()">
+        <textarea x-ref="content" x-bind:value="state"></textarea>
+        <div class="search-results mt-2">
+            <ul>
+                <template x-for="result in searchResults" :key="result.id">
+                    <li @click="state = result.content; editor.setData(result.content)" x-text="result.title" class="cursor-pointer hover:bg-gray-200"></li>
+                </template>
+            </ul>
+        </div>
+    </div>
+    <script src="{{ asset('vendor/ckeditor5/build/ckeditor.js') }}"></script>
+</x-dynamic-component>
