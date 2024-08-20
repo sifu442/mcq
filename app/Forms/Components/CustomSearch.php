320c5f79598fca0b72c822bb90993b8f8c70e505
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-20 16:02:58 +0000

    Aug 20, 2024, 10:02 PM

--- app/Forms/Components/CustomSearch.php
+++ app/Forms/Components/CustomSearch.php
@@ -2,9 +2,35 @@
 
 namespace App\Forms\Components;
 
+use Illuminate\Support\Collection;
 use Filament\Forms\Components\Field;
 
 class CustomSearch extends Field
 {
     protected string $view = 'forms.components.custom-search';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Custom dehydrate logic if needed
+    }
+
+    public function search(callable $callback): self
+    {
+        $this->extraAttributes['searchCallback'] = $callback;
+
+        return $this;
+    }
+
+    public function getSearchResults(): Collection
+    {
+        $callback = $this->extraAttributes['searchCallback'] ?? null;
+
+        if ($callback) {
+            return $callback($this->getState());
+        }
+
+        return collect();
+    }
 }
