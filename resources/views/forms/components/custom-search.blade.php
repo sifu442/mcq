1fca8d0aa463b403706bfb3993a5b07c86b78873
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-20 16:10:45 +0000

    Aug 20, 2024, 10:10 PM

--- resources/views/forms/components/custom-search.blade.php
+++ resources/views/forms/components/custom-search.blade.php
@@ -2,28 +2,34 @@
     <div wire:ignore x-data="{
         state: $wire.entangle('{{ $getStatePath() }}'),
         searchResults: [],
+        initEditor() {
+            ClassicEditor
+                .create($refs.content)
+                .then(editor => {
+                    this.editor = editor;
+
+                    // Listen to changes in the editor
+                    editor.model.document.on('change:data', () => {
+                        $refs.content.value = editor.getData();
+                        this.state = editor.getData(); // Update state with editor data
+                        this.search(); // Perform search on data change
+                    });
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
         search() {
             @this.call('getSearchResults').then(results => {
                 this.searchResults = results;
             });
         }
-    }" x-init="ClassicEditor
-        .create($refs.content)
-        .then(editor => {
-            editor.model.document.on('change:data', () => {
-                $refs.content.value = editor.getData();
-                state = editor.getData();
-                this.search();
-            });
-        })
-        .catch(error => {
-            console.error(error);
-        });">
-        <textarea wire:ignore x-ref="content" x-bind:value="state"></textarea>
+    }" x-init="initEditor()">
+        <textarea x-ref="content" x-bind:value="state"></textarea>
         <div class="search-results mt-2">
             <ul>
                 <template x-for="result in searchResults" :key="result.id">
-                    <li @click="state = result.content" x-text="result.title" class="cursor-pointer hover:bg-gray-200"></li>
+                    <li @click="state = result.content; editor.setData(result.content)" x-text="result.title" class="cursor-pointer hover:bg-gray-200"></li>
                 </template>
             </ul>
         </div>
