8af23cf92c1e1f80bd0ea7c7f0c5945b4721f4e7
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-20 16:03:34 +0000

    Aug 20, 2024, 10:03 PM

--- resources/views/forms/components/custom-search.blade.php
+++ resources/views/forms/components/custom-search.blade.php
@@ -1,8 +1,32 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
->
-    <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
-        <!-- Interact with the `state` property in Alpine.js -->
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
+    <div wire:ignore x-data="{
+        state: $wire.entangle('{{ $getStatePath() }}'),
+        searchResults: [],
+        search() {
+            @this.call('getSearchResults').then(results => {
+                this.searchResults = results;
+            });
+        }
+    }" x-init="ClassicEditor
+        .create($refs.content)
+        .then(editor => {
+            editor.model.document.on('change:data', () => {
+                $refs.content.value = editor.getData();
+                state = editor.getData();
+                this.search();
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });">
+        <textarea wire:ignore x-ref="content" x-bind:value="state"></textarea>
+        <div class="search-results">
+            <ul>
+                <template x-for="result in searchResults" :key="result.id">
+                    <li @click="state = result.content" x-text="result.title"></li>
+                </template>
+            </ul>
+        </div>
     </div>
+    <script src="{{ asset('vendor/ckeditor5/build/ckeditor.js') }}"></script>
 </x-dynamic-component>
