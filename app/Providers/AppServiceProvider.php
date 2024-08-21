31860ceb1e510a338d704d44f0ab41ec1608d9c3
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 10:45:31 +0000

    Aug 21, 2024, 4:45 PM

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Livewire\Livewire;
-use App\Http\Livewire\CkEditorSearch;
+use App\Livewire\CkEditorSearch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
