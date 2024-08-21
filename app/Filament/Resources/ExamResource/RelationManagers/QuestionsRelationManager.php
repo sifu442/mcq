2bc4328c99300eb47ae7fff6a92ef73018b75e6c
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 07:54:11 +0000

    Aug 21, 2024, 1:54 PM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -77,7 +77,7 @@ class QuestionsRelationManager extends RelationManager
             ->filters([])
             ->headerActions([
                 Action::make('advance')
-                ->modalContent(view(''))
+                ->modalContent(view('livewire.custom-modal'))
             ])
             ->actions([Tables\Actions\EditAction::make(), Tables\Actions\DeleteAction::make()])
             ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
