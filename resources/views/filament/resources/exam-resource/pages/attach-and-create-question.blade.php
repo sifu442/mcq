80d2f3680635dd42dcb304a4a12ee078af418074
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 06:50:44 +0000

    Aug 21, 2024, 12:50 PM

--- resources/views/filament/resources/exam-resource/pages/attach-and-create-question.blade.php
+++ resources/views/filament/resources/exam-resource/pages/attach-and-create-question.blade.php
@@ -1,3 +1,20 @@
 <x-filament-panels::page>
 
+<form wire:submit.prevent="save">
+    {{ $this->form }}
+    <div class="mt-4">
+        <h3>Search Results:</h3>
+        <ul>
+            @foreach($searchResults as $result)
+                <li wire:click="selectQuestion({{ $result->id }})">
+                    {{ $result->title }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <x-filament::button type="submit">
+        Save Question
+    </x-filament::button>
+</form>
+
 </x-filament-panels::page>
