4ef371bef316f0eb1b8b3ef6849f0148a8401405
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 06:49:50 +0000

    Aug 21, 2024, 12:49 PM

--- app/Filament/Resources/ExamResource/Pages/AttachAndCreateQuestion.php
+++ app/Filament/Resources/ExamResource/Pages/AttachAndCreateQuestion.php
@@ -2,12 +2,40 @@
 
 namespace App\Filament\Resources\ExamResource\Pages;
 
-use App\Filament\Resources\ExamResource;
+use App\Models\Question;
+use App\Forms\Components\CKEditor;
 use Filament\Resources\Pages\Page;
+use Filament\Forms\Components\Repeater;
+use App\Filament\Resources\ExamResource;
 
 class AttachAndCreateQuestion extends Page
 {
     protected static string $resource = ExamResource::class;
 
     protected static string $view = 'filament.resources.exam-resource.pages.attach-and-create-question';
+
+    public $searchResults = [];
+
+    protected function getFormSchema(): array
+    {
+        return [
+            CKEditor::make('title')
+                ->label('Question Title')
+                ->required()
+                ->afterStateUpdated(function ($state, callable $set) {
+                    $this->searchResults = Question::where('title', 'like', "%$state%")->get();
+                    $set('searchResults', $this->searchResults);
+                }),
+            Repeater::make('searchResults')
+                ->schema([
+                    // Render the question titles as needed
+                ])
+                ->disableLabel(),
+        ];
+    }
+
+    protected function getFormStatePath(): string
+    {
+        return 'state';
+    }
 }
