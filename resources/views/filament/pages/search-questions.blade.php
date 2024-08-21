41c864bb707b0b5f3654ff4d08fa40d158d26e54
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 07:44:07 +0000

    Aug 21, 2024, 1:44 PM

--- resources/views/filament/pages/search-questions.blade.php
+++ resources/views/filament/pages/search-questions.blade.php
@@ -1,13 +1,3 @@
 
-    @if(count($this->searchResults) > 0)
-        <div class="mt-4">
-            <h3 class="text-lg font-medium">Search Results:</h3>
-            <ul class="list-disc pl-5">
-                @foreach($this->searchResults as $result)
-                    <li>{{ $result }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
 
