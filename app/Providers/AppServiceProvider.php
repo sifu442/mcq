b52737ff491286d52ce7c4ee56997ec0554f1514
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 10:37:36 +0000

    Aug 21, 2024, 4:37 PM

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Livewire\Livewire;
+use App\Http\Livewire\CkEditorSearch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Livewire::component('ck-editor-search', CkEditorSearch::class);
     }
 }
