77d36f2c3ba64cab78e862f2d5b8299cd26d252d
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 07:24:45 +0000

    Aug 21, 2024, 1:24 PM

--- app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/ExamResource/RelationManagers/QuestionsRelationManager.php
@@ -69,7 +69,8 @@ class QuestionsRelationManager extends RelationManager
             ])
             ->filters([])
             ->headerActions([
-
+                Action::make('advance')
+                ->modalContent(view('filament.pages.search-questions'))
             ])
             ->actions([Tables\Actions\EditAction::make(), Tables\Actions\DeleteAction::make()])
             ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
