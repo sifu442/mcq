718ed052568f31388ec6464209439720b6b3a44c
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-21 08:13:56 +0000

    Aug 21, 2024, 2:13 PM

--- app/Filament/Pages/SearchQuestions.php
+++ app/Filament/Pages/SearchQuestions.php
@@ -12,25 +12,5 @@ class SearchQuestions extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.search-questions';
-    
-    public $searchResults = [];
 
-    protected function getFormSchema(): array
-    {
-        return [
-            CKEditor::make('title')
-                ->label('Search Title')
-                ->ckeditor()
-                ->afterStateUpdated(fn ($state) => $this->performSearch($state))
-                ->required(),
-            View::make('filament.components.search-results')
-                ->label('Results')
-                ->visible(fn () => count($this->searchResults) > 0),
-        ];
-    }
-
-    public function performSearch($title)
-    {
-        $this->searchResults = Question::where('title', 'like', "%{$title}%")->pluck('title')->toArray();
-    }
 }
