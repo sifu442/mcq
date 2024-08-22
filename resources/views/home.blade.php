85e360a4c7d0435a18149a48f41a398f0d9e8ae2
sifu442 <sifatulislam442@gmail.com>
2024-08-22 04:32:54 +0000

    Aug 22, 2024, 10:32 AM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,23 +103,25 @@
                 aria-labelledby="profile-tab">
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     @foreach ($courses as $course)
-                        <a href="{{ route('course.show', $course->slug) }}" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-                            <div class="p-5">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    {{ $course->title }}
-                                </h5>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট সময়: {{ $course->time_span }} দিন</p>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট পরীক্ষা:{{ $course->time_span }} টি</p>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">কোর্স ফি: {{ $course->price }} টাকা</p>
-                                <div class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
+                        @if (!$course->hidden)
+                            <a href="{{ route('course.show', $course->slug) }}" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                                <div class="p-5">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {{ $course->title }}
+                                    </h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট সময়: {{ $course->time_span }} দিন</p>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট পরীক্ষা:{{ $course->time_span }} টি</p>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">কোর্স ফি: {{ $course->price }} টাকা</p>
+                                    <div class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        @endif
                     @endforeach
                 </div>
             </div>
