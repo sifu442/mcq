951bd112870155551d0d8082521c2fc922685a32
sifu442 <sifatulislam442@gmail.com>
2024-08-23 08:14:48 +0000

    Aug 23, 2024, 2:14 PM

--- app/Filament/Resources/EnrollmentResource/Pages/EditEnrollment.php
+++ app/Filament/Resources/EnrollmentResource/Pages/EditEnrollment.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\EnrollmentResource\Pages;
 
-use App\Filament\Resources\EnrollmentResource;
+use Carbon\Carbon;
 use Filament\Actions;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms\Components\DatePicker;
+use App\Filament\Resources\EnrollmentResource;
 
 class EditEnrollment extends EditRecord
 {
@@ -16,11 +20,38 @@ class EditEnrollment extends EditRecord
             Actions\Action::make('Apply Days')
                 ->label('Apply Days to Routine')
                 ->action('applyDaysToRoutine')
+                ->form([
+                    
+                ])
                 ->requiresConfirmation()
                 ->color('primary'),
             Actions\DeleteAction::make(),
         ];
     }
 
-    
+    public function applyDaysToRoutine()
+    {
+        $data = $this->form->getState();
+
+        $days = (int)($data['day_number'] ?? 0);
+
+        if ($days > 0 && isset($data['routine'])) {
+            foreach ($data['routine'] as $index => $routine) {
+                if (isset($routine['start_time']) && isset($routine['end_time'])) {
+                    $newStartTime = Carbon::parse($routine['start_time'])->addDays($days);
+                    $newEndTime = Carbon::parse($routine['end_time'])->addDays($days);
+
+                    $this->form->fill([
+                        "routine.{$index}.start_time" => $newStartTime,
+                        "routine.{$index}.end_time" => $newEndTime,
+                    ]);
+                }
+            }
+
+            // Save the updated state
+            $this->save();
+        } else {
+            $this->notify('warning', 'Please enter a valid number of days.');
+        }
+    }
 }
