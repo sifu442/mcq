370e969085aa8abb40a8be443117d6a48b55253b
sifu442 <sifatulislam442@gmail.com>
2024-08-24 06:23:32 +0000

    Aug 24, 2024, 12:23 PM

--- resources/views/livewire/exam-page.blade.php
+++ resources/views/livewire/exam-page.blade.php
@@ -1,88 +1,85 @@
-<div x-data="examComponent()" class="flex flex-col lg:flex-row">
-    <!-- Sidebar Contents for Mobile/Tablets -->
-    <div class="lg:hidden p-4 bg-blue-50 dark:bg-gray-800 mb-4">
-        <!-- Timer -->
-        <div class="text-center mb-4">
-            <span class="text-lg font-bold">Time Left</span>
-            <div x-data="countdownTimer({{ $duration * 60 }}, '@lang('messages.minutes')', '@lang('messages.seconds')', '@lang('messages.times_up')')" x-init="startTimer()">
-                <span x-text="timeDisplay" class="block text-2xl font-bold"></span>
-            </div>
-        </div>
-
-        <!-- Exam Details -->
-        <div class="text-lg font-bold text-blue-700 dark:text-gray-200 mb-4">Exam Details</div>
-        <ul class="text-sm text-gray-700 dark:text-gray-300">
-            <li><strong>Total Questions:</strong> {{ $exam->questions->count() }}</li>
-            <li><strong>Answered:</strong> <span x-text="answeredCount"></span></li>
-            <li><strong>Unanswered:</strong> <span x-text="unansweredCount"></span></li>
-        </ul>
-
-        <!-- Exam Name -->
-        <div class="text-center mt-8 text-lg font-bold text-blue-700 dark:text-gray-200">
-            {{ $exam->name }}
-        </div>
-    </div>
-
+<div x-data="examComponent()" class="relative min-h-screen">
     <!-- Main Content -->
-    <div class="flex-grow">
-        <form wire:submit.prevent="submitExam">
-            <ol class="list-decimal list-inside">
-                @foreach ($exam->questions as $question)
-                    <li class="p-2.5 md:p-5">
-                        <span x-html="sanitizeHtml(`{!! $question->title !!}`)"></span>
-                        <ul>
-                            @foreach ($question->options as $index => $option)
-                                <li>
-                                    <div class="flex items-center ps-4 border bg-white border-gray-200 rounded-md dark:border-gray-700 py-2 my-2 drop-shadow-lg">
-                                        <input type="radio"
-                                               value="{{ $option['options'] }}"
-                                               id="option{{ $question->id }}_{{ $index }}"
-                                               name="question{{ $question->id }}"
-                                               class="hidden peer"
-                                               x-on:change="toggleSelection($event, {{ $question->id }}, '{{ $option['options'] }}')"
-                                               x-bind:checked="answers[{{ $question->id }}] === '{{ $option['options'] }}'">
-                                        <label for="option{{ $question->id }}_{{ $index }}"
-                                               class="flex items-center justify-center w-8 h-8 rounded-full border border-gray-300 text-gray-600 peer-checked:bg-blue-600 peer-checked:text-white cursor-pointer">
-                                            {{ chr(65 + $index) }}
-                                        </label>
-                                        <label for="option{{ $question->id }}_{{ $index }}"
-                                               class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                            {{ strip_tags($option['options']) }}
-                                        </label>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @endforeach
-            </ol>
-            <div class="flex justify-center mt-4">
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+    <div class="lg:flex lg:relative">
+        <div class="flex-grow">
+            <form wire:submit.prevent="submitExam">
+                <ol class="list-decimal list-inside">
+                    @foreach ($exam->questions as $question)
+                        <li class="p-2.5 md:p-5">
+                            <span x-html="sanitizeHtml(`{!! $question->title !!}`)"></span>
+                            <ul>
+                                @foreach ($question->options as $index => $option)
+                                    <li>
+                                        <div class="flex items-center ps-4 border bg-white border-gray-200 rounded-md dark:border-gray-700 py-2 my-2 drop-shadow-lg">
+                                            <input type="radio"
+                                                   value="{{ $option['options'] }}"
+                                                   id="option{{ $question->id }}_{{ $index }}"
+                                                   name="question{{ $question->id }}"
+                                                   class="hidden peer"
+                                                   x-on:change="toggleSelection($event, {{ $question->id }}, '{{ $option['options'] }}')"
+                                                   x-bind:checked="answers[{{ $question->id }}] === '{{ $option['options'] }}'">
+                                            <label for="option{{ $question->id }}_{{ $index }}"
+                                                   class="flex items-center justify-center w-8 h-8 rounded-full border border-gray-300 text-gray-600 peer-checked:bg-blue-600 peer-checked:text-white cursor-pointer">
+                                                {{ chr(65 + $index) }}
+                                            </label>
+                                            <label for="option{{ $question->id }}_{{ $index }}"
+                                                   class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                                {{ strip_tags($option['options']) }}
+                                            </label>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ol>
+                <div class="flex justify-center mt-4">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+                </div>
+            </form>
+        </div>
+
+        <!-- Right Sidebar (Visible on Desktop Only) -->
+        <div class="hidden lg:block lg:w-64 bg-blue-50 dark:bg-gray-800 p-4 sticky top-0 h-screen overflow-y-auto">
+            <!-- Timer -->
+            <div class="text-center mb-4">
+                <span class="text-lg font-bold">Time Left</span>
+                <div x-data="countdownTimer({{ $duration * 60 }}, '@lang('messages.minutes')', '@lang('messages.seconds')', '@lang('messages.times_up')')" x-init="startTimer()">
+                    <span x-text="timeDisplay" class="block text-2xl font-bold"></span>
+                </div>
             </div>
-        </form>
+
+            <!-- Exam Details -->
+            <div class="text-lg font-bold text-blue-700 dark:text-gray-200 mb-4">Exam Details</div>
+            <ul class="text-sm text-gray-700 dark:text-gray-300">
+                <li><strong>Total Questions:</strong> {{ $exam->questions->count() }}</li>
+                <li><strong>Answered:</strong> <span x-text="answeredCount"></span></li>
+                <li><strong>Unanswered:</strong> <span x-text="unansweredCount"></span></li>
+            </ul>
+
+            <!-- Exam Name -->
+            <div class="text-center mt-8 text-lg font-bold text-blue-700 dark:text-gray-200">
+                {{ $exam->name }}
+            </div>
+        </div>
     </div>
 
-    <!-- Right Sidebar (Visible on Desktop Only) -->
-    <div class="hidden lg:block lg:w-64 bg-blue-50 dark:bg-gray-800 p-4 sticky">
+    <!-- Floating Bottom for Mobile/Tablets -->
+    <div class="lg:hidden fixed bottom-0 left-0 right-0 bg-blue-50 dark:bg-gray-800 p-4 shadow-lg flex items-center justify-between">
         <!-- Timer -->
-        <div class="text-center mb-4">
+        <div class="text-center">
             <span class="text-lg font-bold">Time Left</span>
             <div x-data="countdownTimer({{ $duration * 60 }}, '@lang('messages.minutes')', '@lang('messages.seconds')', '@lang('messages.times_up')')" x-init="startTimer()">
-                <span x-text="timeDisplay" class="block text-2xl font-bold"></span>
+                <span x-text="timeDisplay" class="block text-xl font-bold"></span>
             </div>
         </div>
 
         <!-- Exam Details -->
-        <div class="text-lg font-bold text-blue-700 dark:text-gray-200 mb-4">Exam Details</div>
-        <ul class="text-sm text-gray-700 dark:text-gray-300">
-            <li><strong>Total Questions:</strong> {{ $exam->questions->count() }}</li>
-            <li><strong>Answered:</strong> <span x-text="answeredCount"></span></li>
-            <li><strong>Unanswered:</strong> <span x-text="unansweredCount"></span></li>
-        </ul>
-
-        <!-- Exam Name -->
-        <div class="text-center mt-8 text-lg font-bold text-blue-700 dark:text-gray-200">
-            {{ $exam->name }}
+        <div class="text-sm text-gray-700 dark:text-gray-300 text-center">
+            <div class="font-bold">Answered</div>
+            <div x-text="answeredCount"></div>
+            <div class="font-bold">Unanswered</div>
+            <div x-text="unansweredCount"></div>
         </div>
     </div>
 </div>
@@ -153,7 +150,6 @@
             },
         }
     }
-
     window.addEventListener('beforeunload', function (e) {
         // Custom message for the confirmation dialog
         const confirmationMessage = 'Are you sure you want to leave? Any unsaved changes will be lost.';
