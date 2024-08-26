abf829549ad226b1f77388f9709b82987ec801ed
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-26 17:23:39 +0000

    Aug 26, 2024, 11:23 PM

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -27,6 +27,22 @@
             Fee: {{ $course->price }} Taka
         </div>
 
+        @if (Auth::check())
+                <button onclick="location.href='{{ route('course.buy', ['course' => $course->id]) }}'"
+                    class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Buy
+                </button>
+            @else
+                <button onclick="location.href='{{ route('register') }}'"
+                    class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Buy
+                </button>
+            @endif
+            <button onclick="location.href='{{ route('course.downloadRoutine', $course->id) }}'"
+                class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Download Routine
+            </button>
+
         <div id="accordion-collapse" data-accordion="open">
             @foreach ($examInfo as $index => $examDate)
                 <h2 id="accordion-collapse-heading-{{ $index }}">
@@ -57,21 +73,7 @@
                 </div>
             @endforeach
 
-            @if (Auth::check())
-                <button onclick="location.href='{{ route('course.buy', ['course' => $course->id]) }}'"
-                    class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Buy
-                </button>
-            @else
-                <button onclick="location.href='{{ route('register') }}'"
-                    class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Buy
-                </button>
-            @endif
-            <button onclick="location.href='{{ route('course.downloadRoutine', $course->id) }}'"
-                class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Download Routine
-            </button>
+
 
         </div>
 
