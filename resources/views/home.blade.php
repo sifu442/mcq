c5776d7216795458b6bb0ff48993fa2057f2f85b
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-26 17:57:04 +0000

    Aug 26, 2024, 11:57 PM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,9 +113,7 @@
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট সময়: {{ $course->time_span }} দিন</p>
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">মোট পরীক্ষা:{{ $course->time_span }} টি</p>
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">কোর্স ফি: <span class="line-through text-red-600">{{ $course->price }}</span> টাকা</p>
-                                    @if 
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">অফার ফি: {{ $course->discounted_price }} টাকা</p>
-                                    @endif
                                     <div class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Read more
                                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
