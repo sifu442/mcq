6175d3949de181dfb03cd8f0da5155896b50d23b
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-08-29 19:48:25 +0000

    Aug 30, 2024, 1:48 AM

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -27,7 +27,6 @@ class DashboardController extends Controller
     $user = Auth::user();
     $now = Carbon::now();
 
-    // Fetch enrollments including course and exams relationships
     $enrollments = Enrollment::with(['course', 'course.exams'])
         ->where('user_id', $user->id)
         ->get();
@@ -44,67 +43,67 @@ class DashboardController extends Controller
         }
 
         if (is_array($routine)) {
-            $startsFrom = $enrollment->starts_from; // Retrieve the starts_from exam ID
-
-            // Filter exams based on starts_from
-            $examsToShow = [];
-            if ($startsFrom) {
-                // Only show exams that come after the specified exam ID within the course
-                $examIdsFromStartsFrom = Course::find($course->id)
-                    ->exams()
-                    ->where('id', '>=', $startsFrom) // Ensure you filter correctly based on ID
-                    ->pluck('id')
-                    ->toArray();
-
-                foreach ($routine as $examRoutine) {
-                    if (in_array($examRoutine['exam_id'], $examIdsFromStartsFrom)) {
-                        $examsToShow[] = $examRoutine;
-                    }
-                }
-            } else {
-                // If starts_from is not set, list all exams under the course
-                $examsToShow = $routine;
-            }
-
-            foreach ($examsToShow as $examRoutine) {
+            foreach ($routine as $examRoutine) {
                 $exam = Exam::find($examRoutine['exam_id']);
                 if ($exam) {
                     $startTime = Carbon::parse($examRoutine['start_time']);
                     $endTime = Carbon::parse($examRoutine['end_time']);
 
-                    $examResponse = ExamResponse::where('user_id', $user->id)
-                        ->where('exam_id', $exam->id)
-                        ->first();
-
-                    if ($examResponse) {
-                        $previousExams[] = (object) [
-                            'id' => $exam->id,
-                            'name' => $exam->name,
-                            'duration' => $exam->duration,
-                            'end_date' => $endTime->format('Y-m-d H:i:s'),
-                            'total_score' => $examResponse->total_score
-                        ];
-                    } elseif ($now->between($startTime, $endTime)) {
-                        $ongoingExams[] = (object) [
-                            'id' => $exam->id,
-                            'name' => $exam->name,
-                            'duration' => $exam->duration,
-                            'end_date' => $endTime->format('Y-m-d H:i:s')
-                        ];
-                    } elseif ($now->lt($startTime)) {
-                        $upcomingExams[] = (object) [
-                            'id' => $exam->id,
-                            'name' => $exam->name,
-                            'duration' => $exam->duration,
-                            'start_date' => $startTime->format('Y-m-d H:i:s')
-                        ];
+                    // Check if the exam is within the course and starts_from condition
+                    if ($enrollment->starts_from) {
+                        if ($exam->id < $enrollment->starts_from) {
+                            $previousExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'end_date' => $endTime->format('Y-m-d H:i:s')
+                            ];
+                        } elseif ($now->between($startTime, $endTime)) {
+                            $ongoingExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'end_date' => $endTime->format('Y-m-d H:i:s')
+                            ];
+                        } elseif ($now->lt($startTime)) {
+                            $upcomingExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'start_date' => $startTime->format('Y-m-d H:i:s')
+                            ];
+                        } else {
+                            $previousExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'end_date' => $endTime->format('Y-m-d H:i:s')
+                            ];
+                        }
                     } else {
-                        $previousExams[] = (object) [
-                            'id' => $exam->id,
-                            'name' => $exam->name,
-                            'duration' => $exam->duration,
-                            'end_date' => $endTime->format('Y-m-d H:i:s')
-                        ];
+                        // Handle logic when starts_from is not set
+                        if ($now->between($startTime, $endTime)) {
+                            $ongoingExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'end_date' => $endTime->format('Y-m-d H:i:s')
+                            ];
+                        } elseif ($now->lt($startTime)) {
+                            $upcomingExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'start_date' => $startTime->format('Y-m-d H:i:s')
+                            ];
+                        } else {
+                            $previousExams[] = (object) [
+                                'id' => $exam->id,
+                                'name' => $exam->name,
+                                'duration' => $exam->duration,
+                                'end_date' => $endTime->format('Y-m-d H:i:s')
+                            ];
+                        }
                     }
                 }
             }
@@ -113,5 +112,6 @@ class DashboardController extends Controller
 
     return view('dashboard.exams', compact('ongoingExams', 'upcomingExams', 'previousExams'));
 }
+
 
 }
