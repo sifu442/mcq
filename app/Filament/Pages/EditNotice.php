3c4dfb2dac90e4928d1334e3e29e9cefa8903357
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-09-01 04:36:39 +0000

    Sep 1, 2024, 10:36 AM

---
+++ app/Filament/Pages/EditNotice.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use App\Forms\Components\CKEditor;
+use Filament\Forms\Concerns\InteractsWithForms;
+
+class EditNotice extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.edit-notice';
+
+    public ?array $data = [];
+
+    use InteractsWithForms;
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                CKEditor::make('text')
+                    ->required(),
+            ])
+            ->statePath('data');
+    }
+
+}
