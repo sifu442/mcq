f98809d8bde1726217a1db5eb8f63d620221a093
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-09-01 04:50:14 +0000

    Sep 1, 2024, 10:50 AM

--- app/Filament/Pages/NoticeBoard.php
+++ app/Filament/Pages/NoticeBoard.php
@@ -2,25 +2,28 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Notice;
 use Filament\Forms\Form;
 use Filament\Pages\Page;
 use Filament\Actions\Action;
 use App\Forms\Components\CKEditor;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Concerns\InteractsWithForms;
 
-class NoticeBoard extends Page
+class NoticeBoard extends Page implements HasForms
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
-    protected static string $view = 'filament.pages.notice-board';
+    use InteractsWithForms;
 
     public ?array $data = [];
 
-    use InteractsWithForms;
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static string $view = 'filament.pages.notice-board';
 
     public function mount(): void
     {
-        $this->form->fill();
+        $notice = Notice::first();
+        $this->data = $notice ? $notice->toArray() : [];
+        $this->form->fill($this->data);
     }
 
     public function form(Form $form): Form
@@ -28,18 +31,31 @@ class NoticeBoard extends Page
         return $form
             ->schema([
                 CKEditor::make('text')
+                    ->label('Notice Text')
                     ->required(),
             ])
             ->statePath('data');
+    }
+
+    public function save(): void
+    {
+        $data = $this->form->getState();
+        $notice = Notice::firstOrCreate(['id' => 1], $data);
+        $notice->update($data);
+
+        \Filament\Notifications\Notification::make()
+            ->success()
+            ->title('Notice updated successfully')
+            ->send();
     }
 
     protected function getFormActions(): array
     {
         return [
             Action::make('save')
-                ->label(__('filament-panels::resources/pages/notice-board.form.actions.save.label'))
+                ->label('Submit')
                 ->submit('save'),
         ];
     }
-
 }
+
