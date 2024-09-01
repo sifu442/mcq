c294094e2fe9a7d90e496cf9005bf540ad6c9037
Sifatul Islam <2014881105@student.bup.edu.bd>
2024-09-01 04:30:32 +0000

    Sep 1, 2024, 10:30 AM

--- database/migrations/2024_09_01_042852_create_notices_table.php
+++ database/migrations/2024_09_01_042852_create_notices_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('notices', function (Blueprint $table) {
             $table->id();
-            $table->id();
+            $table->string('text');
             $table->timestamps();
         });
     }
